02e338aa5aaa099461edbc99ab30aa4ae9a4b41e
GergesHany <gergeshany179@gmail.com>
2025-02-06 05:00:36 +0200

    add authRoutes

--- server/server.js
+++ server/server.js
@@ -2,17 +2,33 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
+const corsOptions = require("./Config/corsOptions");
+const cookieParser = require("cookie-parser");
 
+// Database connection
 const connectDB = require("./Config/dbConn");
 connectDB();
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+app.use(cors(corsOptions));
+app.use(cookieParser());
+
+
+// Routes
+const authRoutes = require("./routes/authRoutes");
 
 const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
+app.use("/auth", authRoutes);
 
+
+// Database connection and server start
 mongoose.connection.once("open", () => {
     console.log("MongoDB connected successfully");    
     app.listen(PORT, () => {
