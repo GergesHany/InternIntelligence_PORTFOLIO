17719e066e4e2049053c99b8a767a8274404e719
GergesHany <gergeshany179@gmail.com>
2025-02-06 04:05:09 +0200

    add middleware verifyJWT

--- server/models/Project.js
+++ server/models/Project.js
@@ -1,14 +1,5 @@
 const mongoose = require("mongoose");
 
-// const ProjectSchema = new Schema({
-//     title: { type: String, required: true },
-//     description: { type: String, required: true },
-//     technologies: [{ type: String, required: true }],
-//     imageUrl: { type: String },
-//     githubUrl: { type: String },
-//     liveUrl: { type: String }
-//   }, { timestamps: true });
-
 const ProjectSchema = new Schema(
     {
         title: {
@@ -33,7 +24,6 @@ const ProjectSchema = new Schema(
             type: String
         }
     }, 
-
     {
       timestamps: true,
     }
